7a204165413dbdae15f734858b7fe8feb03c9eb4
BarneyL01 <barnabaslau@yahoo.com>
2016-02-19 03:02:42 +1100

    selectors for Fight

--- app/fight.component.ts
+++ app/fight.component.ts
@@ -8,7 +8,8 @@ import { FightEngine } from './fight-engine';
 
 @Component({
   selector: 'my-fight',
-  templateUrl : 'app/fight.component.html'
+  templateUrl : 'app/fight.component.html',
+  directives: [] 
   //,
   //styleUrls: ['app/fight.component.css']
 })
@@ -17,6 +18,8 @@ export class FightComponent implements OnInit {
     heroes: Hero[] = [];
     fightResults: Hit[] = [];
     
+    selectedHero1:Hero;
+    selectedHero2:Hero;
     
     constructor(
         private _router: Router,
@@ -25,6 +28,7 @@ export class FightComponent implements OnInit {
 
     ngOnInit() {
          this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+         
     }
     
     startFight()
@@ -47,4 +51,27 @@ export class FightComponent implements OnInit {
                 damage:2, 
                 remainingHp:5 };
     }
+    
+    /*
+        Need this function because selector doesn't seem to bind properly.
+    */
+    getHeroById(id:number){
+        for(let hero of this.heroes){
+            // console.log('hero ', hero.id, ':', hero.name);
+            if(hero.id==id){
+                return hero;
+            }
+        }
+        return ;
+    }
+    setHero1(id:number){
+        // console.log('heroId', id);
+        this.selectedHero1 = this.getHeroById(id);
+        //   console.log('selectedHero1', this.selectedHero1.name);
+    }
+    setHero2(id:number){
+        this.selectedHero2 = this.getHeroById(id);
+    }
+    
+    
 }
