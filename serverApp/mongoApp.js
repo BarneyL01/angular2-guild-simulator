e0ce1a198f440fccfc0646f4509ba9e1b6c30948
BarneyL01 <barnabaslau@yahoo.com>
2016-03-03 16:54:59 +1100

    more refactoring

--- serverApp/mongoApp.js
+++ serverApp/mongoApp.js
@@ -16,25 +16,25 @@ var users = require('./routes/users');
 
 
 
-var app = express();
+var mongoApp = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoApp.set('views', path.join(__dirname, 'views'));
+mongoApp.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//mongoApp.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+mongoApp.use(logger('dev'));
+mongoApp.use(bodyParser.json());
+mongoApp.use(bodyParser.urlencoded({ extended: false }));
+mongoApp.use(cookieParser());
+mongoApp.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+mongoApp.use('/', routes);
+mongoApp.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+mongoApp.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -44,8 +44,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (mongoApp.get('env') === 'development') {
+  mongoApp.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -56,7 +56,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+mongoApp.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -65,4 +65,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = mongoApp;
